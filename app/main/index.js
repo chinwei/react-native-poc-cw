80a1310afcc8cc83dd89ad92528c36b225185943
Diogo Carmo <diogo@totvslabs.com>
2016-09-07 14:00:46 -0300

    Implemented base Navigation Experimental Added Android support

--- app/main/index.js
+++ app/main/index.js
@@ -1,20 +1,61 @@
 import React, { Component } from 'react'
 
 import {
-   NavigatorIOS,
    NavigationExperimental
 } from 'react-native'
 
 import CompaniesView from '../companies'
 import FilterView from '../filter'
 
+const {
+ CardStack: NavigationCardStack,
+ StateUtils: NavigationStateUtils
+} = NavigationExperimental
+
+function createReducer(initialState) {
+  return (currentState = initialState, action) => {
+    switch (action.type) {
+      case 'push':
+        return NavigationStateUtils.push(currentState, {key: action.key});
+      case 'pop':
+        return currentState.index > 0 ?
+          NavigationStateUtils.pop(currentState) :
+            currentState;
+          default:
+            return currentState;
+      }
+   }
+}
+const NavReducer = createReducer({
+  index: 0,
+  key: 'App',
+  routes: [{key: 'Companies'}]
+})
+
 export default class MainView extends Component {
   constructor(props) {
     super(props)
-
+    this.state = {
+      navState: NavReducer(undefined, {})
+    }
   }
 
-  onPress() {
+  _handleAction (action) {
+      const newState = NavReducer(this.state.navState, action);
+      if (newState === this.state.navState) {
+        return false;
+      }
+      this.setState({
+        navState: newState
+      })
+      return true;
+   }
+
+  handleBackAction() {
+     return this._handleAction({ type: 'pop' });
+   }
+
+  _onPress() {
     this.refs.nav.push({
       title: 'Add Items',
       component: FilterView,
@@ -23,17 +64,36 @@ export default class MainView extends Component {
     })
   }
 
+  _renderRoute (key) {
+    if (key === 'Companies') {
+      return (
+        <CompaniesView />
+      );
+    }
+    // if (key === 'About') {
+    //   return <About
+    //           goBack={this.handleBackAction.bind(this)}
+    //           onPress={this._handleAction.bind(this,
+    //           { type: 'push', key: 'Contact' })} />
+    // }
+    // if (key === 'Contact') {
+    //   return <Contact
+    //            goBack={ this.handleBackAction.bind(this)} />
+    // }
+  }
+
+  _renderScene(props) {
+      const ComponentToRender = this._renderRoute(props.scene.route.key)
+
+      return ComponentToRender
+  }
+
   render() {
     return (
-      <NavigatorIOS
-        ref='nav'
-        style={{flex: 1, backgroundColor: 'white'}}
-        initialRoute={{
-          title: 'Companies',
-          component: CompaniesView,
-          rightButtonTitle: 'Filter',
-          onRightButtonPress: () => this.onPress()
-       }} />
+      <NavigationCardStack
+        navigationState={this.state.navState}
+        onNavigate={this._handleAction.bind(this)}
+        renderScene={this._renderScene.bind(this)} />
     )
   }
 }
