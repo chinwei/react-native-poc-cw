09b5cd386d92c3a24960715734dc441a750d2ad5
Chinwei Wong <chinweiwong@Chinweis-MacBook-Pro.local>
2016-09-07 20:11:43 -0700

    extracted some inline styles into a styles object

--- app/companies/index.js
+++ app/companies/index.js
@@ -35,36 +35,18 @@ export default class CompaniesView extends Component {
 
   render() {
     return (
-      <View style={{flex: 1, backgroundColor: 'white', paddingTop: 64}}>
+      <View style={[styles.container]}>
         <View style={{
             backgroundColor: 'white',
             height: 50,
-            flexDirection: 'row',
-            alignItems: 'center',
             borderBottomWidth: 1,
-            borderBottomColor: '#eee'}}>
+            borderBottomColor: '#eee'
+          }}>
           <TextInput
             placeholder= "Search"
-            style={{
-              color: '#333',
-              flex: 1,
-              height: 49,
-              paddingLeft: 20,
-              backgroundColor: 'white',
-              fontSize: 16,
-              fontWeight: '500',
-              borderBottomWidth: 1,
-              borderBottomColor: '#eee',
-              paddingRight: 3}}/>
+            style={styles.textInput}/>
         </View>
-        <View style={{
-            backgroundColor: 'white',
-            height: 40,
-            justifyContent: 'center',
-            flexDirection: 'row',
-            borderBottomWidth: 1,
-            borderBottomColor: '#eee',
-            alignItems: 'center'}}>
+        <View style={[styles.infoBar, styles.flexCenter, styles.borderBottomLight]}>
           <Text style={{color: '#9B9B9B', fontSize: 12, fontWeight: '500', paddingRight: 3}}>654356</Text>
           <Text style={{color: '#9B9B9B', fontSize: 12}}>Companies found (0.02 seconds)</Text>
         </View>
@@ -73,15 +55,8 @@ export default class CompaniesView extends Component {
        ref='listView'
        dataSource={this.state.dataSource}
        renderRow={(rowData) =>
-         <View style={{
-             height: 65,
-             paddingLeft: 20,
-             flexDirection: 'row',
-             alignItems: 'center',
-             borderBottomWidth: 1,
-             borderBottomColor: '#eee'
-           }}>
-           <View style={{flexDirection: 'row', flex: 1}}>
+         <View style={[styles.listViewContainer, styles.flexCenter, styles.borderBottomLight]}>
+           <View style={{flex: 1}}>
              <View style={{paddingLeft: 5, height: 50, flexDirection: 'row', alignItems: 'center', flex: 1}}>
                <Text style={{fontSize: 16, fontWeight: '300'}}>{rowData}</Text>
              </View>
@@ -94,3 +69,42 @@ export default class CompaniesView extends Component {
     )
   }
 }
+
+var styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'white',
+    paddingTop: 64
+  },
+  listViewContainer: {
+    height: 65,
+    paddingLeft: 20,
+    flexDirection: 'row',
+  },
+  flexCenter: {
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  infoBar: {
+    backgroundColor: 'white',
+    height: 40,
+    flexDirection: 'row',
+  },
+  borderBottomLight: {
+    borderBottomWidth: 1,
+    borderBottomColor: '#eee'
+  },
+  textInput: {
+    flex: 1,
+    height: 49,
+    paddingLeft: 20,
+    backgroundColor: 'white',
+    color: '#333',
+    fontSize: 16,
+    fontWeight: '500',
+    borderBottomWidth: 1,
+    borderBottomColor: '#eee',
+    paddingRight: 3
+  }
+
+});
